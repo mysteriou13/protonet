0f0e65bcec6bdf9b33370136e81d5ced79e15d7e
mysteriou <elio007@hotmail.fr>
2017-10-04 15:23:47 +0200

    ajout lien dynamic pour le fichier style.css dans ./site1/index.php

--- site1/index.php
+++ site1/index.php
@@ -1,8 +1,21 @@
 
-<link rel = "stylesheet" href = "../site1/style/style.css">
+<?php
+
+if(file_exists("./site1/style/style.css")){
+
+$style = './site1/style/style.css';
+
+}else{
+
+$style = '../site1/style/style.css';
+
+}
+
+?>
+
+<link rel = "stylesheet" href = "<?php echo $style;?>">
 
 <?php 
-
 
  $monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
 
