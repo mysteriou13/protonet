f703f4eefbbac4d19fe4614212da5fa287818ab7
mysteriou <elio007@hotmail.fr>
2017-10-04 18:07:22 +0200

    ajout lien dynamic  style.css dans ./site1/propos.php

--- site1/propos.php
+++ site1/propos.php
@@ -1,3 +1,19 @@
 
-<link rel = "stylesheet" href = "./site1/style/style.css">
+<?php
+
+if(file_exists("./site1/style/style.css")){
+
+$style = './site1/style/style.css';
+
+}else{
+
+$style = '../site1/style/style.css';
+
+}
+
+?>
+
+<link rel = "stylesheet" href = "<?php echo $style;?>">
+
+
 
