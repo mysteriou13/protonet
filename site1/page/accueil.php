4481d727ef3cd5d0995a4f4df4e317fd5af25954
mysteriou <elio007@hotmail.fr>
2017-10-05 18:10:06 +0200

    supression balise php  tout en haut du fichier ./site1/page/accueil.php

--- site1/page/accueil.php
+++ site1/page/accueil.php
@@ -1,8 +1,4 @@
 
-<?php 
-
-?>
-
 <div id = "conteneur1" style = " width:100% height:100%; background-image: url(<?php echo $background; ?>); background-repeat:no-repeat;  background-size: 100% 100%; " >
 <div id = "c1">
 
