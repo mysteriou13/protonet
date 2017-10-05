f66af8b1d5884323e61681133be18775c465ca16
mysteriou <elio007@hotmail.fr>
2017-10-05 17:25:33 +0200

    deplacement de  page index du site1 dans ./site1/page/accueil.php

---
+++ site1/page/accueil.php
@@ -0,0 +1,73 @@
+
+<div id = "conteneur1" style = " width:100% height:100%; background-image: url(<?php echo $background; ?>); background-repeat:no-repeat;  background-size: 100% 100%; " >
+<div id = "c1">
+
+<center>
+votre titre
+</center>
+
+<div id = "c4">
+
+<div
+onclick = "
+
+<?php 
+
+echo " document.location = ' ";
+
+if($nb == 0){
+
+echo $url1."#site1";
+
+}
+echo "'";
+
+?>
+"
+
+
+>
+accueil
+</div>
+
+<div>
+a propos
+</div>
+
+<div>
+contact
+</div>
+
+</div>
+
+</div>
+
+<div id = "c2" >
+
+<div style = "
+width:70%;
+border:1px solid;
+margin:1%;
+">
+votre text
+</div>
+
+<div
+style = "
+width:28%;
+border:1px solid;
+margin:1%;
+";
+>
+complement text
+</div>
+
+</div>
+
+<div id = "c3">
+mention l&eacute;gal
+</br>
+</ br>
+</div>
+</div>
+
