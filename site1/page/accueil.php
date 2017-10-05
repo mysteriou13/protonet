a334e21723848213be25660bb8094af8b61370a1
mysteriou <elio007@hotmail.fr>
2017-10-05 18:18:19 +0200

    deplacement  haur de page de ./site1/page/accueil.php dans ./site1/section/header.php

--- site1/page/accueil.php
+++ site1/page/accueil.php
@@ -1,46 +1,4 @@
 
-<div id = "conteneur1" style = " width:100% height:100%; background-image: url(<?php echo $background; ?>); background-repeat:no-repeat;  background-size: 100% 100%; " >
-<div id = "c1">
-
-<center>
-votre titre
-</center>
-
-<div id = "c4">
-
-<div
-onclick = "
-
-<?php 
-
-echo " document.location = ' ";
-
-if($nb == 0){
-
-echo $url1."#site1";
-
-}
-echo "'";
-
-?>
-"
-
-
->
-accueil
-</div>
-
-<div>
-a propos
-</div>
-
-<div>
-contact
-</div>
-
-</div>
-
-</div>
 
 <div id = "c2" >
 
