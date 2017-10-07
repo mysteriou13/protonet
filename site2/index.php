d07d61687ad6116fd44969ddd712cf181c6efa76
mysteriou <elio007@hotmail.fr>
2017-10-07 18:31:05 +0200

    correction lien vers index et vers site le site2 dans ./site2/index.php

--- site2/index.php
+++ site2/index.php
@@ -17,22 +17,61 @@ include_once("../des.php");
 
 }
 
-$monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
+
+ $monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
 
  $url = $monUrl;
 
-$nb = mb_substr_count($monUrl, "site2");
+ $nb = mb_substr_count($monUrl, "site2");
 
+ $nb1 = mb_substr_count($monUrl, "#");
+
+ $nb2 = mb_substr_count($monUrl, "index.php");
+
+ $nb3 = mb_substr_count(getcwd(),"/site2");
+
+ $nb4 = str_replace("site1","",getcwd());
+
+ $nb5 = mb_substr_count($monUrl,"/site2/index.php");
+ 
+ 
 if($nb == 1){
 
 $des1 = "cliquer pour retourner a l'index";
 
-$url = str_replace('site2/','#site2',$monUrl);;
+$url = str_replace('site2/','index.php#site1',$monUrl);;
 }else{
 
-$url = $url."site2/";
+$url = $url."/site2/index.php";
 
 }
+
+if($nb2 == 1){
+
+$c = explode('/index.php',$url);
+
+$url = $c[0]."/site2/index.php";
+
+if($nb == 1 ){
+
+$url = $c[0]."/index.php#site2";
+
+}
+
+}
+
+if(isset($_GET['parametre'])){
+
+
+$url = str_replace("index.php#site2","",$url);
+
+$url = $url."?parametre=".$_GET['parametre']."#site2";
+
+
+}
+
+
+
 
 $lien = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
 
@@ -43,6 +82,7 @@ $header = $page."/site2/section/header.php";
 
 $accueil = $page."/site2/section/accueil.php";
 $footer = $page."/site2/section/footer.php";
+
 
 ?>
 <center>
