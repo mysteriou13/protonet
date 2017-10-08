63d9e4fcaacf6486a5affcb647d52d8a0d2f011b
mysteriou <elio007@hotmail.fr>
2017-10-08 23:32:48 +0200

    deplacement link dynamic du site2 vers l'index  dans la class lien dans ./fonction/url.php

--- fonction/url.php
+++ fonction/url.php
@@ -1,7 +1,102 @@
 <?php 
 
+class lien{
+
 function url(){
 
+ $monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
+
+ $nbsite = 2;
+
+ $parametre = "parametre".$nbsite;
+ 
+ $site = "site";
+ 
+ $url = $monUrl;
+
+ $nb = mb_substr_count($monUrl, $site);
+
+ $nb1 = mb_substr_count($monUrl, "#");
+ 
+ $nb2 = mb_substr_count($monUrl, "index.php");
+
+ $nb3 = mb_substr_count(getcwd(),"/site".$nbsite);
+
+ $nb4 = str_replace($site.$nbsite,"",getcwd());
+
+ $nb5 = mb_substr_count($monUrl, $site."/"."/index.php");
+ 
+ $c = explode("site2",$url);
+
+
+if($nb1 == 0){
+if($nb2 == 1){
+if($nb3 == 0){
+
+ if(isset($_GET[$parametre])){
+ $a = "index.php?parametre"."=".$site.$_GET[$parametre]; 
+
+ $a1 = "?".$parametre."=".$_GET[$parametre];
+
+ $url = str_replace($a1,"site".$nbsite."/" ,$url);
+
+  $url = str_replace("index.php","",$url);
+  
+
+  $url = $url."index.php?".$parametre."=".$_GET[$parametre];
+}else{
+
+
+ $url = str_replace("/index.php","",$url);
+  $url = $url."/site".$nbsite;
+
+}
+
+}
+}
+
+
+if($nb2 == 1){
+if($nb3 == 1){
+$url = $c[0]."index.php#site".$nbsite;
+
+}
+}
+
+if($nb2 == 0){
+if($nb3 == 1){
+$url = str_replace("site".$nbsite,"",$c[0]);
+
+$url = $url."index.php#site".$nbsite;
+
+}
+if($nb3 == 0){
+
+$url = $c[0];
+
+ $a = explode("?",$c[0]);
+
+ $url = $a[0]."site".$nbsite;
+}
+}
+}
+
+
+if(isset($_GET[$parametre])){
+
+
+$url = str_replace("index.php#site".$nbsite,"",$url);
+
+$url = $url."?".$parametre."=".$_GET[$parametre]."#site".$nbsite;
+
+
+}
+
+$lien = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+return $url;
+
+}
 }
 
 ?>
--- site2/index.php
+++ site2/index.php
@@ -17,97 +17,13 @@ include_once("../des.php");
 
 }
 
+include_once("/opt/lampp/htdocs/protonet/fonction/url.php");
 
- $monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
+$lien2 = new lien();
 
- $url = $monUrl;
-
- $nb = mb_substr_count($monUrl, "site2");
-
-  $nb1 = mb_substr_count($monUrl, "#");
- 
-
-  $nb2 = mb_substr_count($monUrl, "index.php");
-
-
-  $nb3 = mb_substr_count(getcwd(),"/site2");
-
-
- $nb4 = str_replace("site2","",getcwd());
-
- $nb5 = mb_substr_count($monUrl,"/site2/index.php");
- 
- $c = explode("site2",$url);
-
-
-if($nb1 == 0){
-if($nb2 == 1){
-if($nb3 == 0){
-
- if(isset($_GET['parametre2'])){
- $a = "index.php?parametre2=".$_GET['parametre2']; 
-
- $a1 = "?parametre2=".$_GET['parametre2'];
-
- $url = str_replace($a1,"site2/",$url);
-
-  $url = str_replace("index.php","",$url);
-  
-
-  $url = $url."index.php?parametre2=".$_GET['parametre2'];
-}else{
-
-
- $url = str_replace("/index.php","",$url);
-  $url = $url."/site2";
-
-}
-
-}
-}
-
-
-if($nb2 == 1){
-if($nb3 == 1){
-$url = $c[0]."index.php#site2";
-
-}
-}
-
-if($nb2 == 0){
-if($nb3 == 1){
-$url = str_replace("site2","",$c[0]);
-
-$url = $url."index.php#site2";
-
-}
-if($nb3 == 0){
-
-$url = $c[0];
-
- $a = explode("?",$c[0]);
-
- $url = $a[0]."site2";
-}
-}
-}
-
-
-if(isset($_GET['parametre2'])){
-
-
-$url = str_replace("index.php#site2","",$url);
-
-$url = $url."?parametre2=".$_GET['parametre2']."#site2";
-
-
-}
-
-
-
+$url =  $lien2->url();
 
 $lien = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
-
 
 $page = str_replace("/site2","",getcwd());
 
