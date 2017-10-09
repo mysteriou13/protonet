49dcb4d369a629ccb71b461b7556dfa5d98898d2
mysteriou <elio007@hotmail.fr>
2017-10-09 19:55:40 +0200

    ajout margin-left:1% dans le div du fichier ./site3/section/accueil.php

--- site3/section/accueil.php
+++ site3/section/accueil.php
@@ -1,5 +1,5 @@
 
-<div style = "border:1px solid black; width:80%; margin-right:1%; margin-top:1%; ">
+<div style = "border:1px solid black; width:80%; margin-right:1%;  margin-left:1%; margin-top:1%; ">
 
 votre text
 
