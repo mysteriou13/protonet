37f274ac688b9d966836724d5a0d3373df5922e4
mysteriou <elio007@hotmail.fr>
2017-10-10 18:10:33 +0200

    deplamcement du div avec id site4a6 dans ./site4/section/footer.php

--- site4/index.php
+++ site4/index.php
@@ -65,10 +65,4 @@ votre text
 </div>
 </div>
 
-<div id = "site4a6">
-
-<center>
-mention l&eacute;gal
-</center>
-</div>
 </div>
---
+++ site4/section/footer.php
@@ -0,0 +1,8 @@
+
+<div id = "site4a6">
+
+<center>
+mention l&eacute;gal
+</center>
+</div>
+
