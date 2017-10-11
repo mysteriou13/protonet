8dd8ec57387aac4cbaeaf763a5bd6569b0f245ed
mysteriou <elio007@hotmail.fr>
2017-10-11 23:52:16 +0200

    include fonction url dans ./site5/index.php

--- site5/index.php
+++ site5/index.php
@@ -16,9 +16,22 @@ include_once("../des.php");
 
 }
 
-$monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
 
-$url = $monUrl;
+
+$lien3 = getcwd();
+
+$lien3 = str_replace("site4","",$lien3);
+
+$lien3 = $lien3."/fonction/url.php";
+
+include_once($lien3);
+
+$lien2 = new lien();
+
+$url =  $lien2->url("5");
+
+$lien = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
 $page = str_replace("/site5","",getcwd());
 $header = $page."/site5/section/header.php";
 $footer = $page."/site5/section/footer.php";
