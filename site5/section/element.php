0cb7e14b9e34a8cdbdfd1062281366fc07a0ca0d
mysteriou <elio007@hotmail.fr>
2017-10-12 11:25:12 +0200

    ajout text dans ./site5/section/element.php

--- site5/section/element.php
+++ site5/section/element.php
@@ -1,5 +1,23 @@
 
 <div id = "site5a4">
+<?php 
+$text = null;
+if(isset($_GET['parametre5']) && !empty($_GET['parametre5'])){
+if($_GET['parametre5'] == 2){
+
+$text = "<center>contact</center>";
+
+}
+
+
+}else{
+
+$text = "<center>accueil</center>";
+
+}
+
+echo $text;
+?>
 
 </div>
 
