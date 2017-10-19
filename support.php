6e365792192fc1fc12f631ddf521dc4358bd0190
mysteriou <elio007@hotmail.fr>
2017-10-19 20:15:29 +0200

    include en tete dans ./support.php

--- support.php
+++ support.php
@@ -1,5 +1,22 @@
 <html>
 <body>
+<?php 
+session_start();
 
+$d = "corsicanet";
+
+include_once("./head.php");
+
+include_once("./header.php");
+
+$des1 = "cliquer pour agrandir";
+
+ $monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+
+$nb = mb_substr_count($monUrl, "#");
+
+
+?>
 </body>
 </html>
