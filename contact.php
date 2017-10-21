b9297a409f89a2ce5f9af4ee08330237bef6cae5
mysteriou <elio007@hotmail.fr>
2017-10-21 16:28:45 +0200

    remplace input  avec name message  par balise textarea  dans ./contact.php

--- contact.php
+++ contact.php
@@ -26,7 +26,10 @@ $nb = mb_substr_count($monUrl, "#");
 </br>
 <input type = "text" name = "email">
 </br>
-<input type = "text" name = "message">
+<textarea name="textarea" style = "height:50%; width:50%">
+
+</textarea>
+
 </form>
 
 </div>
