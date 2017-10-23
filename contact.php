800e0ebc6bbb6c40bcc0387eabc3649c06d47b07
mysteriou <elio007@hotmail.fr>
2017-10-23 19:55:23 +0200

    ajout unpload fichier dans le  form du div avec id contact dans ./contact.php

--- contact.php
+++ contact.php
@@ -12,12 +12,14 @@ include_once("./header.php");
 
 $des1 = "cliquer pour agrandir";
 
- $monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-   $c =  mysqli_connect("localhost","root","elio13790","corsicanet"); 
+  $monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+  $c =  mysqli_connect("localhost","root","elio13790","corsicanet"); 
   $mysqli = new mysqli("localhost", "root", "elio13790", "corsicanet");
 
 $name = null;
 $email = null;
+$fichier = null;
 
 $nb = mb_substr_count($monUrl, "#");
 
@@ -27,7 +29,7 @@ $nb = mb_substr_count($monUrl, "#");
 <div id = "contact">
 
 
-<form method = "POST" action = "<?php echo $_SERVER['PHP_SELF']."#contact"?>">
+<form method = "POST" action = "<?php echo $_SERVER['PHP_SELF']."#contact"?>"  enctype="multipart/form-data">
 <center>
 <div style = "
 color:white;
@@ -92,7 +94,47 @@ echo "format email non valide";
 </div>
 
 <div>
-<input type = "file" name = "fichier">
+piece jointe<input  name = "picture" type = "file">
+<?php 
+if (isset($_FILES['picture']) AND $_FILES['picture']['error'] == 0){
+
+$uploads_dir = '/opt/lampp/htdocs/protonet/';
+$tmp_name = $_FILES["picture"]["tmp_name"];
+
+$id = "SELECT id FROM commentaire ORDER BY ID DESC LIMIT 0,1";
+
+$id1 = $mysqli->query($id);
+
+$row = $id1->fetch_assoc();
+
+$name = $_FILES["picture"]["name"];
+
+$rename1 = explode(".",$name);
+
+$filea =$rename1[0].$row['id'].".".$rename1[1];
+
+$rename = $filea;
+
+$link = getcwd()."/".$rename;
+
+$infosfichier = pathinfo($_FILES['picture']['name']);
+$extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png','odt');
+$extension_upload = $infosfichier['extension'];
+
+                if (in_array($extension_upload, $extensions_autorisees)){
+
+move_uploaded_file($tmp_name, "$uploads_dir/$rename");
+
+ $fichier = 1;
+
+}else{
+
+echo "error upload fichier";
+
+}
+}
+
+?>
 </div>
 
 <div style = "
@@ -107,17 +149,24 @@ message
 </br>
 <?php 
 
- $resul = $name+$email;
+$resul = $name+$email+$fichier;
+$resul = $resul+$fichier;
 
-  if($resul == 2){
+  if($resul == 3){
 
- $pseudo = $mysqli->real_escape_string($_POST['name']); 
+ $pseudo = $mysqli->real_escape_string(htmlspecialchars($_POST['name'])); 
 
- $message = $mysqli->real_escape_string($_POST['textarea']); 
+ $message = $mysqli->real_escape_string(htmlspecialchars($_POST['textarea'])); 
 
- $mail = $mysqli->real_escape_string($_POST['email']);
+ $mail = $mysqli->real_escape_string(htmlspecialchars($_POST['email']));
 
-  $mysqli->query("INSERT into commentaire VALUES(NULL,'$pseudo','$mail','$message')");
+ $lien = $mysqli->real_escape_string(htmlspecialchars($link));
+
+ $name1 = $mysqli->real_escape_string(htmlspecialchars($name));
+ 
+ $rename2 = $mysqli->real_escape_string(htmlspecialchars($rename));
+
+  $mysqli->query("INSERT into commentaire VALUES(NULL,'$pseudo','$mail','$message','$lien','$name1','$rename2')");
   
    }
 
