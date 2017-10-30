9f85d2d2aba13cd9fde4bd20a014eed50cb67aa8
mysteriou <elio007@hotnail.fr>
2017-10-30 23:02:45 +0100

    ajout requete sql pour  trouver le pseudo dans la table commande dans ./commande.php

--- commande.php
+++ commande.php
@@ -7,6 +7,16 @@ $d = "mes produits -protonet";
 
 include_once("./head.php");
 include_once("./header.php");
+
+$login = $connect->real_escape_string($_SESSION['pseudo']);
+
+$produit = "SELECT count(*)pseudo FROM commande WHERE pseudo = '$login' ";
+
+$selectproduit1 = $connect->query($produit);
+
+$selectproduit2 =$selectproduit1->fetch_assoc();
+
+
 ?>
 </br>
 
