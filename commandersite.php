4c32ca7c58ec269d2062d9d5cd6fde3e3717a61f
mysteriou <elio007@hotnail.fr>
2017-10-31 19:16:09 +0100

    ajout style color:white et font-size:2em dans le div  du div avec id contact dans ./commandersite.php

--- commandersite.php
+++ commandersite.php
@@ -7,11 +7,13 @@ include("./hautepage.php");
 
 <div id = "contact">
 
+<div style = "color:white; font-size:2em;">
+
 demander un devis
 
+</div>
+
 <form method = "POST" action = "<?php echo $S_SERVER['PHP_SELF']?>">
-
-
 </form>
 
 </div>
