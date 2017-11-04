634e92af232c3c611427d66b37cd022302e436d2
mysteriou <elio007@hotnail.fr>
2017-11-04 18:30:51 +0100

    rejonte graphic du  div display-flex dans ./commandersite.php

--- commandersite.php
+++ commandersite.php
@@ -13,21 +13,42 @@ demander un devis
 </center>
 </div>
 
-<div style = "display:flex">
+<center>
 
-<div>
+<div style = "
+height:100%;
+display: flex;
+justify-content: space-around;
+
+">
+
+<div style = "
+height:40%;
+width:40%;
+background-color:gray;
+
+">
 
 boutique en ligne
 
 </div>
 
-<div>
+<div
+style = "
+height:40%;
+width:40%;
+background-color:gray;
+"
+
+>
 site vitrine
 </div>
 
 </div>
+</center>
 
 </div>
+
 <?php 
 include_once("footer.php");
 ?>
