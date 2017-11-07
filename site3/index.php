b512810786a5939aafb1e87d2b8d7254b74e13dd
mysteriou <elio007@hotnail.fr>
2017-11-07 11:18:11 +0100

    correction lien css et meta pour  ecran mobile dans ./site3/index.php

--- site3/index.php
+++ site3/index.php
@@ -1,5 +1,7 @@
 
-<link rel  = "stylesheet" href = "./site3/style/style.css">
+<link rel  = "stylesheet" href = "http://www.vecchionet.com/site3/style/style.css">
+
+ <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
 
 <?php
 
