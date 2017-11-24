28d81f6ff4406e39b1fe4a13845b0c06d1772142
mysteriou <elio007@hotnail.fr>
2017-11-24 17:56:55 +0100

    ajout border 1px solid black dans le form avec id formcommande dans ./offre.php

--- offre.php
+++ offre.php
@@ -82,7 +82,7 @@ include_once("./formlogin.php");
 
 <div id  = "commande" style = "display:none; font-size:1em; ">
 
-<form  method = "POST"  action = "" id = "formcommande">
+<form  method = "POST"  action = "" id = "formcommande" style = "border:1px solid black">
 
 site vitrine <input type = "checkbox" id = "vitrine">
 </br>
