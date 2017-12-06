5bb900b4a494049ab9c6a2798a02dd363c6d5177
mysteriou <elio007@hotnail.fr>
2017-12-06 02:18:41 +0100

    creation fonction email

---
+++ email.php
@@ -0,0 +1,21 @@
+
+<?php 
+
+function email(){
+
+
+     $to  = 'elio007@hotmail.fr';
+     $subject = 'le sujet';
+     $message = 'Bonjour !';
+     $headers = 'From: vecchionnet.com' . "\r\n" .
+     'Reply-To: webmaster@example.com' . "\r\n" .
+     'X-Mailer: PHP/' . phpversion();
+
+     mail($to, $subject, $message, $headers);
+
+
+
+}
+
+?>
+
