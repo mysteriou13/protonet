bbb7eaf0b4e04009086159d48a339612d68985e0
mysteriou <elio007@hotnail.fr>
2017-12-06 22:00:21 +0100

    ajout variable expediteur dans la variable  headers dans ./email.php

--- email.php
+++ email.php
@@ -1,11 +1,11 @@
 
 <?php 
 
-function email($to,$subject,$message,$destinataire){
+function email($to,$subject,$message,$destinataire,$expediteur){
 
 
       $headers = 'From: '.$destinataire."\r\n" .
-     'Reply-To: webmaster@example.com' . "\r\n" .
+     'Reply-To: '.$expediteur . "\r\n" .
      'X-Mailer: PHP/' . phpversion();
 
      mail($to, $subject, $message, $headers);
