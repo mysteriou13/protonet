9e2eeaec84e3e660b71b36f68ee8e57ad2d2b6de
mysteriou <elio007@hotnail.fr>
2017-12-22 07:55:02 +0100

    ajout balise center  pour les inputs dans le form dans ./changepass.php

--- changepass.php
+++ changepass.php
@@ -11,12 +11,13 @@ include_once("./hautepage.php");
 <div id = "conteneur2">
 
 <form  method = "post" action = "<?php echo $_SERVER['PHP_SELF']?>">
-
+<center>
 <input type = "text" name = "pass">
 </br>
 <input type = "text" name = "confirpass">
 </br>
 <input type = "submit" >
+</center>
 </form>
 
 </div>
