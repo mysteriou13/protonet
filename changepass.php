8997c3f272d9e550065f30aa42d66e6338118b88
mysteriou <elio007@hotnail.fr>
2017-12-24 09:31:00 +0100

    ajout change mot de pass dans ./changepass.php

--- changepass.php
+++ changepass.php
@@ -12,13 +12,67 @@ include_once("./hautepage.php");
 
 <form  method = "post" action = "<?php echo $_SERVER['PHP_SELF']?>" style = "color:white; font-size:2em">
 <center>
-<label> change de mot pass</label>
+<label> changement de mot pass</label>
 </br>
-<label>nouveau mot pass </label> <input type = "text" name = "pass">
 </br>
-<label> confirmer mot de pass</label><input type = "text" name = "confirpass">
+<label>nouveau mot pass </label> <input type = "password" name = "Pass">
 </br>
-<input type = "submit" >
+<label> confirmer mot de pass</label><input type = "password" name = "confirpass">
+</br>
+<input type = "submit">
+</br>
+<?php 
+
+
+if(isset($_POST['Pass']) && !empty($_POST['Pass'])){
+if(isset($_POST['confirpass']) && !empty($_POST['confirpass'])){
+if($_POST['confirpass'] != $_POST['Pass']){
+
+echo " les champs ne sont pas indentique ";
+
+
+}
+}
+}
+
+?>
+<?php
+
+if(isset($_POST)){
+if(isset($_POST['Pass']) && !empty($_POST['Pass'])){
+if(isset($_POST['confirpass']) && !empty($_POST['confirpass'])){
+if(isset($_GET['email']) && !empty($_GET['email'])){
+if($_POST['confirpass'] != $_POST['Pass']){
+
+
+$email = $_GET['email'];
+
+$email = htmlspecialchars($email);
+
+$email = $connect->real_escape_string($email);
+
+$pass = $_POST['Pass'];
+
+$pass = htmlspecialchars($pass);
+
+$pass =  sha1($pass);
+
+$pass  = $connect->real_escape_string($pass);
+
+$update = "UPDATE membre  SET pass = '$pass' WHERE email = '$email'";
+
+$connect->query($update);
+
+echo "votre  nouveau mot de pass est enregistr&eacute;";
+
+}
+}
+}
+}
+}
+
+
+?>
 </center>
 </form>
 
