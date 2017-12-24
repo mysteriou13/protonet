c183ac95c8cc955cf796fecd43d08af4bc56bb16
mysteriou <elio007@hotnail.fr>
2017-12-24 19:09:37 +0100

    ajout div de mise en page  dans ./suiviproduit.php

--- suiviproduit.php
+++ suiviproduit.php
@@ -6,14 +6,35 @@
 include("./hautepage.php");
 ?>
 
-<div id = "conteneur2">
+<div id = "conteneur2" style = "padding-top:1%;">
 
 <?php
 
-
 $produit = htmlspecialchars($_GET['produit']);
 
+$produit = "SELECT * FROM commande WHERE id = '$produit'";
+
+$produit2 = $connect->query($produit);
+
+$produit3 = $produit2->fetch_assoc();
+
+
  ?>
+
+<div  style = "display:flex; border:1px solid white; justify-content: space-around; ">
+
+<div style = " display:flex; justify-content:space-between; border:1px solid; margin:1%; ">
+
+
+</div>
+
+<div>
+
+
+</div>
+
+</div>
+
 
 </div>
 
