3ea9980c5e83bcb09e2b5113e85b42f66e3e0e28
mysteriou <elio007@hotnail.fr>
2018-02-17 23:49:03 +0100

    ajout liste des  service dans ./index.php

--- index.php
+++ index.php
@@ -20,9 +20,57 @@ $nb = mb_substr_count($monUrl, "#");
 
 ?>
 
+<div style = "color:white">
+
+<div id = "conteneur2">
+
+<div style = "font-size:3em">
+liste des services
+</div>
+
+</div>
 
 
+<div id = "conteneur2">
 
+<div style = "font-size:3em">
+framadate
+
+</div>
+
+</div>
+
+<div id = "conteneur2" >
+
+<div style = "font-size:3em">
+framacalc
+
+</div>
+
+</div>
+
+
+<div id = "conteneur2" >
+
+<div style = "font-size:3em">
+framaboard
+
+</div>
+
+</div>
+
+
+<div id = "conteneur2">
+
+<div style = "font-size:3em">
+framapad
+
+</div>
+
+</div>
+
+</div>
+</div>
 
 </body>
 </html>
