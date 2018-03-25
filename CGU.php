c2933b5bdc8b65c0b976ecb4ab8b99957966a6af
mysteriou <elio007@hotnail.fr>
2018-03-25 15:59:45 +0200

    include header.php et head.php dans CGU.php

--- CGU.php
+++ CGU.php
@@ -1,6 +1,27 @@
 <html>
 <body>
 
+<?php 
+session_start();
+
+$d = "corsicanet";
+
+
+
+$des1 = "cliquer pour agrandir";
+
+ $monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+?>
+
+<?php 
+
+include("./head.php");
+
+include("./header.php");
+
+?>
+
 
 </body>
 </html>
