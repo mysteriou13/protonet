1eab6a9a4f23cf3e4d9f742f4417564aac51bdcf
mysteriou <elio007@hotnail.fr>
2018-03-31 20:52:20 +0200

    ajout test dans presentation.php

--- presentation.php
+++ presentation.php
@@ -1,6 +1,24 @@
 
 <div id = "conteneur2">
 <center>
+<h1>
+vecchionet  c'est:
+</h1>
+
+<ul>
+<li>
+un Hébergeur : j' administre  sur un serveur d&eacute;dier  des programmes pour naviagateur web.
+</li>
+
+<li>
+des logiciels libre : tout l'infrastruture et  les logiciels que je proposons sont logiciel libre(comme la libert&eacute; d'expression)
+</li>
+
+
+</ul>
+
+</br>
+
 </center>
 </div>
 
