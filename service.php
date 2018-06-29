31d4ae5806c529254472eb9eea7a467f12f3abd7
mysteriou <elio007@hotmail.fr>
2018-06-29 11:04:46 +0200

    changer desgin dans service.php

--- service.php
+++ service.php
@@ -4,9 +4,9 @@
 
 <div id = "conteneur2">
 
-<div>
+<div style = "background-color:blue; color:white;">
 <p style = "font-size:3em">
-<a href = "https://vecchioemo.vecchionet.com" target="_blank" > framemo</a>
+<a href = "https://vecchioemo.vecchionet.com" style = "color:white;" target="_blank" > framemo</a>
 </p>
 <p style = "font-size:2em">
 tableau de post-it collaboratif
@@ -18,44 +18,50 @@ tableau de post-it collaboratif
 
 
 
-<div>
 <div id = "conteneur2">
-<p  style = "font-size:3em"><a href = "https://www.vecchionet.com/kanboard-1.2.1" target="_blank" > kanboard </a> </p>
+<div  style = "background-color:blue; color:white;">
+<p  style = "font-size:3em"><a href = "https://www.vecchionet.com/kanboard-1.2.1" style  = "color:white;" target="_blank" > kanboard </a> </p>
 
 <p style = "font-size:2em">
 gestionaire de projet
 </p>
 </div>
+</div>
 
  <div id = "conteneur2">
-<p style = "font-size:3em">
-<a href = "https://www.vecchionet.com/framavectoriel/svg-editor.html" target="_blank" >SVG-edit</a>
+<div style = "background-color:blue; color:white;">
+<p style = "font-size:3em;">
+<a href = "https://www.vecchionet.com/framavectoriel/svg-editor.html" style = "color:white" target="_blank" >SVG-edit</a>
 </p>
 <p style = "font-size:2em">
-logiciel d'&eacute;dition d'mages et de retouche de photo
+logiciel d'&eacute;dition d'images et de retouche de photo
 </p>
+</div>
 </div>
 
 <div id = "conteneur2">
 
+<div style = "background-color:blue; color:white;">
 <p style = "font-size:3em">
-<a href = "https://www.vecchionet.com/framadate" target="_blank" >framadate</a>
+<a href = "https://www.vecchionet.com/framadate" style = "color:white"; target="_blank" >framadate</a>
 </p>
 <p style = "font-size:2em">
 organiser vos render-vous 
 </p>
-
+</div>
 </div>
 
 <div id = "conteneur2">
-   <p style = "font-size:3em">
-<a href = "http://vecchionet.com/pad.php" target="_blank" >vecchiopad</a>
+  
+<div style = "background-color:blue; color:white;" >
+ <p style = "font-size:3em">
+<a href = "https://www.vecchionet.com/pad.php" style = "color:white;" target="_blank" >vecchiopad</a>
 </p>
 <p style = "font-size:2em">
 editeur de text colaboratif
 </p>
 
-  
+ </div>
 </div>
 
 </center>
