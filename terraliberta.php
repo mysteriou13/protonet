013cad1a4f6d2076ad1c687753518933a4cfd46b
mysteriou <elio007@hotmail.fr>
2018-07-21 10:23:49 +0200

    ajout fichier terraliberta.php

---
+++ terraliberta.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php
+include_once("head.php");
+include_once("header.php");
+
+?>
+
+<iframe src = "https://terraliberta.vecchionet.com" style = "height:100%; width:100%;">
+
+</iframe>
+
+</body>
+</html>
+
+
