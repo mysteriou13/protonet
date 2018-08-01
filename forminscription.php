fd317479350bd890473ec18fb23c73c0c208d41f
mysteriou <elio007@hotmail.fr>
2018-08-01 10:48:42 +0200

    correction ajustement formulaire de connection

--- forminscription.php
+++ forminscription.php
@@ -1,17 +1,36 @@
 
-<div id = "b">
+<div id = "b"  style = "width:100%;">
 <div id = "login">
 <center>
 <form action = "<?php echo $_SERVER['PHP_SELF']; ?>" method = "POST">
+
+<div>
+
+<div>
 connection
+</div>
 </br>
+
+<div>
 pseudo <input name = "pseudo" type = "text">
+</div>
+
+<div>
 password <input name = "pass" type = "password">
+</div>
+
+<div>
 <input type = "submit" value = "connection">
+</div>
+
  <a id = "b" href = "inscription.php">inscription</a>
 </br>
 
+<div>
 <a id= "b" href = "oublipass.php"> mot de pass oubli&eacute; </a>
+</div>
+
+</div>
 
 </form>
 </center>
