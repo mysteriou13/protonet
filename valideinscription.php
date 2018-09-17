1c63b7a70a35e54383cf0599b36ad3f16bd02ce5
mysteriou <eli007@hotmail.fr>
2018-09-17 13:43:30 +0200

    ajout redirection vers index apres inscription dans ./valideinscription.php

--- valideinscription.php
+++ valideinscription.php
@@ -10,7 +10,8 @@
  <div id = "b" style = "margin-top:2%; font-size:2em;">
 
  <center>
- inscription reussie  
+
+ inscription reussie redirection vers l'accueil dans 5 seconde  <?php sleep(5);  header('Location: index.php')?> 
 
  </center>
 
