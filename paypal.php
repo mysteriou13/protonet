400833f496af7b5b93528436e7adb876b65732ec
mysteriou <eli007@hotmail.fr>
2018-09-18 22:27:30 +0200

    ajout iframe vers la page paypal de terraliberta dans ./paypal.php

--- paypal.php
+++ paypal.php
@@ -2,7 +2,27 @@
  include_once("head.php");
  include_once("header.php");
  include_once("iframe.php");
+
+ $nameserver = $_SERVER['SERVER_NAME'];
+
+ $paypal = "/terraliberta/paypal";
+
+if($_SERVER['SERVER_NAME'] == "localhost" ){
+
+ $u = "http";
+
+}else{
+
+ $u = "https";
+
+}
+
+$u = $u."://".$nameserver."/terraliberta/paypal/index.php";
+
+
 ?>
 
 
+<iframe src = "<?php echo $u;?>" style = " border:1px solid cyan; margin-top:2%; height:100%; width:100%;">
 
+</iframe>
