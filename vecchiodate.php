5207fb91ab523c03ec4edad4f3998ec591b41580
mysteriou <eli007@hotmail.fr>
2018-09-29 23:12:11 +0200

    change lien dans le iframe dans ./vecchiodate.php

--- vecchiodate.php
+++ vecchiodate.php
@@ -5,7 +5,7 @@ include_once("head.php");
 include_once("header.php");
 ?>
 
-<iframe src = "https://www.vecchionet.com/framadate/" style = "height:100%; width:100%; margin-top:2%;">
+<iframe src = "https://framadate.vecchionet.com/" style = "height:100%; width:100%; margin-top:2%;">
 
 
 </iframe>
