87fb4eea38cd92bc59058a08f7619e72056a5427
mysteriou <eli007@hotmail.fr>
2018-09-30 09:14:28 +0200

    ajout iframe framadate dans  ./framadate.php

--- framadate.php
+++ framadate.php
@@ -5,5 +5,9 @@ include("head.php");
 include("header.php");
 ?>
 
+<iframe  src = "https://framadate.vecchionet.com" style = "border:1px; cyan; height:100%; width:100%;">
+
+</iframe>
+
 </body>
 </html>
