d0057fbf278ef12151736819db6e0fbb95c7fab9
mysteriou <eli007@hotmail.fr>
2018-10-02 07:14:35 +0200

    ajout margin-top au iframe   dans ./framadate.php

--- framadate.php
+++ framadate.php
@@ -5,7 +5,7 @@ include("head.php");
 include("header.php");
 ?>
 
-<iframe  src = "https://framadate.vecchionet.com" style = "border:1px; cyan; height:100%; width:100%;">
+<iframe  src = "https://framadate.vecchionet.com" style = " margin-top:1%; border:1px; cyan; height:100%; width:100%;">
 
 </iframe>
 
