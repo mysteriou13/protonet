d85169321545a6ceaca290004047367dfd00823f
mysteriou <eli007@hotmail.fr>
2018-10-08 21:02:01 +0200

    ajout fonction listdiv

--- listfichier.php
+++ listfichier.php
@@ -7,6 +7,8 @@ include("head.php");
 
 include("header.php");
 
+include("divlistement.php");
+
 ?>
 
 
@@ -28,61 +30,37 @@ liste des documents
 
 <div id = "ongletliste" style = "  margin-left:2%; display:flex;">
 
-<div onclick = "changebackgroundcolor('ongletpad','darkblue','blue')" id= "ongletpad" style = "display:<?php echo $display?>; color:white; padding:0%; font-size:1.5em;  margin-right:0.5%; solid white; background-color:darkblue ;">
+<div onclick = "changebackgroundcolor('ongletpad','darkblue','blue')" id= "ongletpad" style = "display:<?php echo $display?>; color:white; padding:0%; font-size:1.5em;  margin-right:0.5%; solid white; background-color:blue ;">
 
 pad
 
 </div>
 
-<div id= "ongletcalc" style = "display:<?php echo $display;?>; color:white; padding:1%; font-size:1.5em;  background-color:blue">
+<div id= "ongletcalc" style = "display:<?php echo $display;?>; color:white; padding:1%; font-size:1.5em;  background-color:darkblue">
 calc
 </div>
 
 </div>
- 
-<?php 
 
 
-$pseudo = $mysqli->real_escape_string($_SESSION['pseudo']);
+<div  id = "listcalc"style ="display:none;">
 
+<?php
 
-$list = "SELECT * FROM url WHERE pseudo = '$pseudo'";
-
-$list2 = $mysqli->query($list);
-
-$type = null;
-
-$link = null;
-
-$lien = null;
-
-$name = null;
-
- while($liste3 = $list2->fetch_assoc() ){
-
-echo "<div style = 'margin-left:2%; margin-right:2%; font-size:1.5em; background-color:blue; color:white;  display:flex;  width:1OO%; justify-content:space-around;  margin-bottom:1%;'>";
-
-echo "<div style = ' padding-left:5%;width:100%;' >";
-echo "type";
-echo "</div>";
-
-echo "<div style = 'width:100%;'>";
-echo $liste3["type"]; 
-echo "</div>";
-
-echo "<div style = 'width:100%;'>";
-$url = $liste3["url"];
-
-echo "<a href = '$url' id = 'b'>"; echo $liste3["name"];echo "</a>";
-echo "</div>";
-
-echo "</div>";
-
- 
-}
+ listdiv($mysqli,"calc");
 
 ?>
 
+</div>
+
+<div id  = "listpad" style = "display:block;">
+<?php 
+
+ listdiv($mysqli,"pad");
+
+?>
+
+</div>
 
 </body>
 </html
