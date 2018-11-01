79ee1579c719a20600a4d3ad1da024c1f2e4f972
mysteriou <eli007@hotmail.fr>
2018-11-01 16:52:46 +0100

    change desgin inscription.php

--- inscription.php
+++ inscription.php
@@ -30,68 +30,78 @@ $date = $mysqli->real_escape_string($date);
 </h1>
 
 
- <div  style = "width:100%; color:white;  display:flex; flex-wrap:wrap  justify-content:space-around;">
-
-   <div  style = "width:60%; padding:1%; background-color:blue; border:2px solid white; border-radius:25px; margin-right:1%; font-size:2em;">
+ <div  style = " width:100%; color:white;  display:flex; flex-wrap:wrap  justify-content:space-around;">
+<div style  = " width:100%; background-color:darkblue; border:2px solid white; border-radius:25px; margin-right:1%; font-size:2em" >
+   <div>
  
-    <center>
+    <center style = "background-color:blue; margin:3%; border:2px solid  white; border-radius:25px; ">
       <strong>
-<u>
      basic 0 euro par mois
-</u>
      </strong>
     </center>
 
      <div>
 
-        <div style = " border:2px solid white; border-radius:25px">
-        1 mois de premium
+        <div style = " background-color:blue; margin:3%;  border:2px solid white; border-radius:25px">
+        <center> 1 mois de premium </center>
         </div>
        
-        <div style =  "border:2px solid white; border-radius:25px; ">
-        classement des  document dans le classeur
-        </div>
- 
+        <div style = "background-color:blue; margin:3%; border:2px solid white; border-radius:25px; ">
+        <center>
+        rangement des  document dans le classeur
+       </center>
+     </div>
+  
      </div>
 
-    </div>
+   <div style = "background-color:blue; margin:3%;  padding-right:5%;  border:2px solid white; border-radius:25px;">
+<center>
+   <a href = "" style  = "color:white;"> devenir basic </a>
+</center> 
+ </div>
 
-  <div style = "display:flex; flex-wrap:wrap; background-color:blue; padding:1%; border:2px solid white; border-radius:25px;  font-size:2em;">
+   </div>
+ 
+   </div>
 
-<div style = "width:100%;">
- <center>
+  <div style = "width:100%; display:flex; flex-wrap:wrap; background-color:darkblue; margin:3%;  border:2px solid white; border-radius:25px;  font-size:2em;">
+
+<div>
+
+ <center style = "background-color:blue; margin:3%; border:2px solid  white; border-radius:25px; ">
 
  <strong>
 
-<u>
 premium 5 euro/mois
-</u>
- </center>
-</strong>
-</div>
 
-  <div>
+</strong>
+ </center>
+
+  <div style = "width:100%;">
    
-     <div style = "border:2px solid white; border-radius:25px">
-     tout les avantage du basic
-   
+     <div style = "background-color:blue; margin:3%;border:2px solid white; border-radius:25px">
+   <center>  
+   tout les avantage du basic
+  </center> 
  </div>
   
-   <div style = "border:2px solid white; border-radius:25px">
-   accés a la terraliberta(espace de travail numérique et colaboratif);
-   </div>   
-   
+   <div style = "background-color:blue; margin:3%;border:2px solid white; border-radius:25px">
+<center>
+   accés a la terraliberta(espace de travail numérique et colaboratif)
+  </center>
+ </div>   
+
+
+     <div style = "background-color:blue; margin:3%; border:2px solid white; border-radius:25px;">
+<center>
+   <a href = "" style  = "color:white;"> devenir premium </a>
+</center>
+ </div>
+  
+ 
   </div>
 
-
-      <div>
-
-      </div>
-  
-   </div>
-
-   </div>
-
+ </div>
    </div>
 
 <script>
