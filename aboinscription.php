60ab8e9c847718ec0ca3406b83e2dffcc9af0b9c
mysteriou <eli007@hotmail.fr>
2018-11-01 19:54:32 +0100

    ajout fichier aboinscription.php

---
+++ aboinscription.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+
+<?php 
+
+$d = "corsicanet";
+
+$des1 = "cliquer pour agrandir";
+
+ $monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+?>
+
+<?php 
+
+include("./head.php");
+
+include("./header.php");
+
+
+
+?>
+</body>
+</html>
+
+
+
