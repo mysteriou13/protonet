69f9dc0cdd144d82b0158b4ead2a601157fd00cb
mysteriou <eli007@hotmail.fr>
2018-11-20 07:23:26 +0100

    ajout code pour suprimer les calc dans redis.php

--- redis.php
+++ redis.php
@@ -1,20 +1,80 @@
 
  <?php
-  require_once'./vendor/predis/predis/src/Autoloader.php';
- include("../admin/connect.php");
+ 
+  include("../admin/connect.php");
 
-  Predis\Autoloader::register();
+ 
+ 
+$redis = new Redis(); 
+ 
+$redis->connect('127.0.0.1', 6379);
 
-   try {
-    $redis = new Predis\Client();
-    echo "connect sucess";
-  
-}catch (Exception $e) {
-    echo "Couldn't connected to Redis";
-    echo $e->getMessage();
+$date = date("dmy");
+
+$day = substr($date,0,2);
+
+$mouth = substr($date,2,2);
+ 
+$year = substr($date,4,2);
+
+$datelink = "SELECT * FROM calc";
+
+$datelink1 = $mysqli->query($datelink);
+
+
+while($datalink2 = $datelink1->fetch_assoc()){
+
+$name = $datalink2['name'];
+
+$name = $mysqli->real_escape_string($name);
+
+$url = "SELECT * FROM url WHERE url like '%$name%'";
+
+$url1 = $mysqli->query($url);
+
+$url2 = $url1>fetch_assoc();
+
+$linkday = substr($datalink2['date'],0,2);
+
+$linkmouth = substr($datalink2['date'],2,2);
+
+$linkyear = substr($datalink2['date'],4,2);
+
+$id = $datalink2['id'];
+
+$id = $mysqli->real_escape_string($id);
+
+$delete = "DELETE* FROM url WHERE url like '%$name%'"; 
+
+$file = $datalink2['name'];
+
+$redis->delete($del1); 
+
+if($day > $linkday){
+if($linkmouth ==  $mouth){
+if($linkyear == $year){
+if($url2['type'] == "calc"){
+
+ $del1 = "snapshot-".$file."_formdata";
+
+ $del2 = "audit-".$file;
+
+ $del3 = "log-".$file;
+
+ $del4 = "snapshot-"$file;
+
+ $redis->delete($del1);
+ $redis->delete($del2);
+ $redis->delete($del3);
+
+ $mysqli->query($elete);
+ 
+}
+}
+}
 }
 
-
+}
 
 ?>
 
