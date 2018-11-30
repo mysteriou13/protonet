9da0407f2aa283208f6f33153f1dbde6bfa19927
mysteriou <eli007@hotmail.fr>
2018-11-30 16:32:09 +0100

    creation d'une class deletefile qui herite redis et ajout d 'un fonction delelecal  dans ./delete.php

--- delete.php
+++ delete.php
@@ -1,8 +1,29 @@
 <?php 
 
-class deletefile (){
+class  deletefile extends Redis {
+
+function deletecal($file){
+
+ $this->connect('127.0.0.1', 6379);
+
+ $del1 = "snapshot-".$file."_formdata";
+
+ $del2 = "audit-".$file;
+
+ $del3 = "log-".$file;
+
+ $del4 = "snapshot-".$file;
+
+ $this->delete($del1);
+ $this->delete($del2);
+ $this->delete($del3);
+ $this->delete($del4);
+
 
 
 }
 
+}
+
 ?>
+
