ff95c527aafcc7bfcc9a0ede0b4d6a5638c855b1
mysteriou <eli007@hotmail.fr>
2019-01-01 19:43:20 +0100

    remplace liste des document par mes documents dans ./divfichier.php

--- divfichier.php
+++ divfichier.php
@@ -2,6 +2,6 @@
 <div style ="background-color:blue; border:2px solid white; border-radius:25px; padding:1%; font-size:1.5em; margin-left:1%; margin-right:1%;">
 
 
-<a id = "b" href = "listfichier.php" > liste des  documents </a>
+<a id = "b" href = "listfichier.php" > mes  documents </a>
 
 </div>
