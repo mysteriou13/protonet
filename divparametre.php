3ca0afce21bdc97e77733c5abe010f4df0e7b4f7
mysteriou <eli007@hotmail.fr>
2019-01-09 02:16:45 +0100

    supression table style et ajout class parametre dans ./divparametre.php

--- divparametre.php
+++ divparametre.php
@@ -1,6 +1,5 @@
 
-<div style = "background-color:blue; border:2px solid white; border-radius:25px; padding:1%;font-size:1.5em;">
-
+<div class= "parametre">
 <a id = "b" href = "parametre.php"> parametre </a>
 
 </div>
