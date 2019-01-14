e5e6d2ff50cb419e17e135fcc619917c591db298
mysteriou <eli007@hotmail.fr>
2019-01-14 13:54:08 +0100

    include divmembre.php dans ./header.php

---
+++ divmembre.php
@@ -0,0 +1,27 @@
+<div  class = "divdisplay" id = "newcalc">
+<a id = "b"  href = "calc.php"> nouveau tableur </a>
+
+</div>
+
+
+<div class = "divdisplay" id = "newpad">
+<a id = "b" href = "pad.php">nouveau pad </a>
+
+ </div>
+
+
+<div class = "divdisplay" id = "sondage">
+
+<a id = "b" href = "https://framadate.vecchionet.com/">sondage</a>
+</div>
+
+<div class = "divdisplay" id  = "edition">
+<a id = "b" href = "./edit.php">edition d'image</a>
+</div>
+
+
+<div class = "divdisplay" id = "projet">
+<a id = "b" href = "https://kanboard.vecchionet.com">gestion de projet</a>
+</div>
+
+
--- header.php
+++ header.php
@@ -130,28 +130,12 @@ include("divinscription.php");
 
 if(isset($_SESSION['pseudo'])){
 
-include("newpad.php");
-
-include("newcalc.php");
+include("divmembre.php");
 
 }
 
 ?>
 
-
-<div class = "divdisplay" id = "sondage">
-
-<a id = "b" href = "https://framadate.vecchionet.com/">sondage</a>
-</div>
-
-<div class = "divdisplay" id  = "edition">
-<a id = "b" href = "./edit.php">edition d'image</a>
-</div>
-
-
-<div class = "divdisplay" id = "projet">
-<a id = "b" href = "https://kanboard.vecchionet.com">gestion de projet</a>
-</div>
 
 
 <?php
