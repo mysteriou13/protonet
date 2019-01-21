aa9c7047264721c5ddb0bd78e1495f6057c2bf09
mysteriou <eli007@hotmail.fr>
2019-01-21 22:52:57 +0100

    ajout creation de sondage dans opensondage.php

--- opensondage.php
+++ opensondage.php
@@ -1,4 +1,54 @@
 <?php 
 include("hautepage.php");
 
+$sondage =  $protole.$domaine."opensondage.php";
+
+$typesondage =  null;
+
+$polldisplay = "none";
+
+$date = "https://framadate.vecchionet.com/create_poll.php?type=";
 ?>
+
+
+<div  style = "display:flex; justify-content:center;">
+<div>
+<form action = "<?php echo $sondage."?type=autre" ?>" method = "POST">
+
+<input type = "submit" id = "b3" value = "sondage classique">
+
+</form>
+</div>
+
+<div>
+<form action = "<?php echo $sondage."?type=date"?>" method = "POST">
+
+<input type = "submit" id = "b3" value = "sondage avec date">
+
+</form>
+</div>
+
+</div>
+
+
+<?php
+
+if(isset($_GET['type']) && !empty($_GET['type'])){
+
+$polldisplay = "block";
+
+$typesondage = htmlspecialchars($_GET['type']);
+
+
+}
+
+?>
+
+<div style = "display:<?php echo $polldisplay;?>">
+
+<iframe  src = "<?php echo $date?>">
+
+</iframe>
+
+</div>
+
