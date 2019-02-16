a6e11c532523475e45ccb0fc919b32bf38f139d9
mysteriou <eli007@hotmail.fr>
2019-02-16 03:21:47 +0100

    ajout question 2 dans ./faq.php

--- faq.php
+++ faq.php
@@ -37,6 +37,21 @@ vecchionet se  démarque par le respect total de la vie privé et la liberté de
 
 </div>
 
+<button onclick = "changedisplay('q2')">
+a qui s'adress vecchionet?
+
+</button>
+
+<div id = "b">
+
+<div id = "q2" style = "display:none; font-size:1.5em;">
+
+vecchionet s'adress aux particuliers, TPE,PME, association et les administrations qui désire  qui s'émencipé des fournisseur des service en ligne qui base leur modele economique sur le profilage publicitaire
+
+</div>
+
+</div>
+
 </div>
 
 </div>
