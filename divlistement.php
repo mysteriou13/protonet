7184a48f82655b316575b12aacde6bd884ec5f16
mysteriou <eli007@hotmail.fr>
2019-02-24 18:54:42 +0100

    ajout  lien  vers deletefile.php

--- divlistement.php
+++ divlistement.php
@@ -44,6 +44,8 @@ while($liste3 = $list2->fetch_assoc() ){
  $calc1 = $mysqli->query($calc);
 
  $calc2 = $calc1->fetch_assoc();
+ 
+ $delete = "deletefile.php?type=".$type."&name=$name";
 
 
 echo "<div style = 'margin-left:2%; margin-right:2%; font-size:1.5em; background-color:blue; color:white;  display:flex;  width:1OO%; justify-content:space-around;  margin-bottom:1%;'>";
@@ -78,6 +80,12 @@ echo "<div>";
 echo "date de fin:"; echo substr($calc2['date'], 0, 2); echo "/"; echo substr($calc2['date'], 2, 2); echo "/"; echo substr($calc2['date'], 4, 2);
 
 echo "</div>";
+
+echo "<div>";
+
+echo "&nbsp; &nbsp"; echo "<a href = '$delete' >"; echo "supprimer"; echo "</a>";
+
+echo "</dibv>";
 
 echo "</div>";
 
