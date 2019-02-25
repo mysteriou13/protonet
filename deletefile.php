750eccead707fb67a0df50cdaef72478df472f8d
mysteriou <eli007@hotmail.fr>
2019-02-25 14:16:50 +0100

    recuperation de get type, get name, get id dans ./deletefile.php

--- deletefile.php
+++ deletefile.php
@@ -3,6 +3,13 @@
 <?php 
 include("header.php");
 include("head.php");
+
+$type = htmlspecialchars($_GET['type']);
+
+$name = htmlspecialchars($_GET['name']);
+
+$id = htmlspecialchars($_GET['id']);
+
 ?>
 
 </body>
