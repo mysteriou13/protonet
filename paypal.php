732a241c5ee31cb9cd55554e272fdedc965bcb3c
mysteriou <eli007@hotmail.fr>
2019-03-11 11:01:07 +0100

    ajout notify_url dans les bouton paypal dans ./paypal.php

--- paypal.php
+++ paypal.php
@@ -106,6 +106,8 @@ duréer abonnement:</br>
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="MNMXGZ7B73BKN">
 <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+<input type="hidden" name="notify_url" value="https://vecchionet.com/payvalide.php">
+
 <input type="hidden" name="item_number" value="1">
 <input type = "hidden" name  = "pseudo" value = "<?php echo $_SESSION['pseudo']?>">
 <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
@@ -118,6 +120,8 @@ duréer abonnement:</br>
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="JRL9BP97TLT7A">
+   <input type="hidden" name="notify_url" value="https://vecchionet.com/payvalide.php">
+
 
 <input type="hidden" name="item_number" value="3">
 <input type = "hidden" name  = "pseudo" value = "<?php echo $_SESSION['pseudo']?>">
@@ -133,6 +137,8 @@ duréer abonnement:</br>
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="B553KWLBMTE2J">
 <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+   <input type="hidden" name="notify_url" value="https://vecchionet.com/payvalide.php">
+
 <input type="hidden" name="item_number" value="6">
 <input type = "hidden" name  = "pseudo" value = "<?php echo $_SESSION['pseudo']?>">
 
@@ -146,6 +152,7 @@ duréer abonnement:</br>
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="DMV3D7WQGKG2J">
 <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+<input type="hidden" name="notify_url" value="https://vecchionet.com/payvalide.php">
 
 <input type="hidden" name="item_number" value="12">
 <input type = "hidden" name  = "pseudo" value = "<?php echo $_SESSION['pseudo']?>">
