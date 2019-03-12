099f07e6f52165e9412ebd59e28f1f18a9b5fe73
mysteriou <eli007@hotmail.fr>
2019-03-12 13:21:09 +0100

    deplacement des boutonpaypal dans ./boutonpayal.php

---
+++ boutonpaypal.php
@@ -0,0 +1,63 @@
+<div  id = "a1" style = "display:none">
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="MNMXGZ7B73BKN">
+<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+<input type="hidden" name="notify_url" value="https://vecchionet.com/payvalide.php">
+
+<input type="hidden" name="item_number" value="1">
+<input type = "hidden" name  = "pseudo" value = "<?php echo $_SESSION['pseudo']?>">
+<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+
+</form>
+
+</div>
+
+<div style = "display:none"  id = "a2">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="JRL9BP97TLT7A">
+   <input type="hidden" name="notify_url" value="https://vecchionet.com/payvalide.php">
+
+
+<input type="hidden" name="item_number" value="3">
+<input type = "hidden" name  = "pseudo" value = "<?php echo $_SESSION['pseudo']?>">
+
+<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div>
+
+<div style = "display:none"  id = "a3">
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="B553KWLBMTE2J">
+<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+   <input type="hidden" name="notify_url" value="https://vecchionet.com/payvalide.php">
+
+<input type="hidden" name="item_number" value="6">
+<input type = "hidden" name  = "pseudo" value = "<?php echo $_SESSION['pseudo']?>">
+
+<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div>
+
+<div style = "display:none" id = "a4">
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="DMV3D7WQGKG2J">
+<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, le réflexe sécurité pour payer en ligne">
+<input type="hidden" name="notify_url" value="https://vecchionet.com/payvalide.php">
+
+<input type="hidden" name="item_number" value="12">
+<input type = "hidden" name  = "pseudo" value = "<?php echo $_SESSION['pseudo']?>">
+
+<img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div>
+</center>
+</form>
+</div>
