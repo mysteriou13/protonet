befcaa0430ab697f33d3094a531ea066ccfbed19
mysteriou <eli007@hotmail.fr>
2019-04-03 12:18:23 +0200

    retrait balise saute de ligne au button creation de pad dans ./pad.php

--- pad.php
+++ pad.php
@@ -31,21 +31,20 @@ vecchiopad est un instance etherpad-lite, editeur de text  permetant de travaill
 
 </p>
 
-
+<p>
 <button  onClick = "parent.location = 'etherpad.php?pad=+7days&durer=7jour'"> pad 1  semaine </button>
 
 <button  onClick = "parent.location = 'etherpad.php?pad=+1month&durer=1 mois'" > pad 1  mois </button>  
 
   <button onClick = "parent.location = 'etherpad.php?pad=+3month&durer=3 mois'" > pad 3  mois </button>
 
- </br>
-</br>
-</br>
- 
+</p>
+
+<p>
   <button  onClick = "parent.location = 'etherpad.php?pad=+6month&durer=6 mois'" > pad 6  mois </button>
  
   <button  onClick = "parent.location = 'etherpad.php?pad=+1year&durer=1 ans'"> pad 1 ans </button>
-
+</p>
  
  </p>
 </center>
