7f5642c2fd3569080ec15d29198ce272ab3b64ca
mysteriou <eli007@hotmail.fr>
2019-06-10 14:03:59 +0200

    supression code inutile dans  inscription.php et supression du fichier aboinscription.php

--- inscription.php
+++ inscription.php
@@ -36,92 +36,14 @@ $wrap = "nowrap";
 
 </h1>
 
-
- <div  style = " color:white;  display:flex; flex-wrap:<?php echo $wrap;?>;  justify-content:space-around;">
-<div style  = " 
-<?php 
-
-if($mobile == false){
-
-echo "margin-right:1%;";
-
-}
-
-?>
-background-color:darkblue; border:2px solid white; border-radius:25px;  font-size:2em" >
-   <div>
- 
-    <center style = "background-color:blue; margin:3%; border:2px solid  white; border-radius:25px; ">
-      <strong>
-     basic 0 euro par mois
-     </strong>
-    </center>
-
-     <div>
-
-        <div style = " background-color:blue; margin:3%;  border:2px solid white; border-radius:25px">
-        <center> 1 mois de premium </center>
-        </div>
-       
-        <div style = "background-color:blue; margin:3%; border:2px solid white; border-radius:25px; ">
-        <center>
-        rangement des  documents dans le classeur
-       </center>
-     </div>
-  
-     </div>
-
-   <div style = " background-color:blue;  padding-right:5%;  border:2px solid white; border-radius:25px;">
-<center>
-   <a href = "aboinscription.php?mode=basic" style  = "color:white;"> devenir basic </a>
-</center> 
- </div>
-
-   </div>
- 
-   </div>
-
-
-  <div style = " display:flex; flex-wrap:wrap; background-color:darkblue;   border:2px solid white; border-radius:25px;  font-size:2em;">
-
 <div>
-
- <center style = "background-color:blue; margin:3%; border:2px solid  white; border-radius:25px; ">
-
- <strong>
-
-premium 5 euro/mois
-
-</strong>
- </center>
-
-  <div >
-   
-     <div style = "background-color:blue; margin:3%;border:2px solid white; border-radius:25px">
-   <center>  
-   tout les avantage du basic
-  </center> 
- </div>
-  
-   <div style = "background-color:blue; margin:3%;border:2px solid white; border-radius:25px">
 <center>
-   accés a la terraliberta(espace de travail numérique et colaboratif)
-  </center>
- </div>   
+<?php
 
-
-     <div style = "background-color:blue; margin:3%; border:2px solid white; border-radius:25px;">
-<center>
-   <a href = "aboinscription.php?mode=premium" style  = "color:white;"> devenir premium </a>
+include("forminscription.php");
+?>
 </center>
- </div>
-  
- 
-  </div>
-
- </div>
-   </div>
-
+</div>
 <script>
 
 var a = document.getElementById("connection");
