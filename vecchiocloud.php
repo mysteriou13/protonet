021a20cf5db121ad2f83768840865818eff50f70
mysteriou <eli007@hotmail.fr>
2019-07-16 18:27:13 +0200

    ajout web reponsive dans ./vecchiocloud.php

--- vecchiocloud.php
+++ vecchiocloud.php
@@ -9,19 +9,47 @@ include("head.php");
 include("header.php");
 ?>
 
-<div  id ="b4" style = "margin-top:1%;padding:1%; display:flex; ">
+<div  id ="b4" style = "margin-top:1%;padding:1%; 
 
-<div style = "width:50%;">
-<img src ="./image/nextcloud.png"   style ="border-radius:25px; width:100%;">
-</div>
+<?php
 
-<div style ="margin:1%; padding-left:2%;  border:3px solid white; border-radius:25px; width:50%">
+if($mobile == 0){
+
+echo "display:flex;";
+
+}
+
+?>
+ ">
+
+<div style ="margin:1%; padding-left:2%;  border:3px solid white; border-radius:25px; 
+<?php 
+
+if($mobile == 0){
+
+echo "width:50%";
+
+}
+
+?>
+
+">
+
+
 
 <strong>vecchiobox</strong> un service d'hebergement chiffr&eacute; de fichier synchronis&eacute;s entre vos ordinateurs, tablettes, mobiles
 
 et que vous pouvez partager facilement avec vos contacts.
 
 </div>
+
+
+<div style = "width:100%;">
+
+<img src ="./image/nextcloud.png"   style ="border-radius:25px; width:100%;">
+
+</div>
+
 
 
 
