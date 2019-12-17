83a925b87eeb779728297be9a1935deedbe2699c
mysteriou <elio007@hotmail.fr>
2019-12-17 08:49:50 +0100

    alignement des element dans legal.php

--- legal.php
+++ legal.php
@@ -10,36 +10,51 @@ include_once("head.php");
 include_once("header.php");
 ?>
 
- <div id = "b3" style = "display:<?php echo $dismobile?>">
+ <div style = "display:<?php echo $dismobile?>">
 
- <center>
- <a id = "legal" href = "legal.php"> haute de page </a> 
- </center>
-
-</div>
-
-<div id = "b4" >
-
-<div >
 <center>
-responsable du site
-<div>
-</div>
-</br>
-nom :massa
-</br>
-</br>
-pr&eacute;nom: anthony
-</br>
-</br>
-email:mrmassaanthony@gmail.com
-</br>
-</br>
-adress:casa d'arutoli rte de muratello bat i apt 43 20137 porto-vecchio
-</div>
-
+<button id ="b4" class ="divheader">
+ <a id = "legal" href = "legal.php"> haute de page </a> 
+</button>
 </center>
+</div>
+</br>
 
+<div>
+<center id = "b4" class= "divheader">
+responsable du site
+</center>
+</div>
+</br>
+
+<div id ="b4">
+<center>
+nom :massa
+</center>
+</div>
+
+</br>
+
+<div id = "b4">
+<center>
+pr&eacute;nom: anthony
+</center>
+</div>
+</br>
+
+<div id= "b4">
+<center>
+email:mrmassaanthony@gmail.com
+</center>
+</div>
+</br>
+
+<div id = "b4">
+<center>
+adress:casa d'arutoli rte de muratello bat i apt 43 20137 porto-vecchio
+</center>
+</div>
+</center>
 </div>
 
 </body>
