1fbf96e16162a37c143f730634db55b2c435bbd2
mysteriou <elio007@hotmail.fr>
2019-12-22 15:23:18 +0100

    remplace input submit par button submit  dans le formulaire de connection ./formconnection.php

--- formconnection.php
+++ formconnection.php
@@ -22,7 +22,7 @@
 </br>
 
 <div >
-<center><input  id = "b3" type = "submit"   value = "connection"></center>
+<center><button  id = "b4" class ="button" type = "submit"   value = "connection">connection </button></center>
 </div>
 </br>
 
