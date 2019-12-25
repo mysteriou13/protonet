3fd969540736ae93728bc6fc0e1a581cc3337c68
mysteriou <elio007@hotmail.fr>
2019-12-25 10:41:47 +0100

    supression lien haute de page dans ./divindex.php

--- divindex.php
+++ divindex.php
@@ -18,17 +18,6 @@ $link =  "http://".$_SERVER['SERVER_NAME']."/document";
 
 ?>
 
-<div id = "b3" style = "display:<?php echo $dismobile;?>;">
-
-<div id = "index">
-<center>
-
-<a href = "index.php"> haut de page </a>
-
-</center>
-</div>
-
-</div>
 
 <iframe  src  ="<?php echo $src?>" style = "height:100%; width:100%; background-color:blue">
 
