980bf93887e7e2a7df7e5122fc57b15aaf499e20
mysteriou <elio007@hotmail.fr>
2020-01-01 00:21:23 +0100

    supression boucle localhost dans ./divmembre.php

--- divmembre.php
+++ divmembre.php
@@ -6,20 +6,6 @@
 </div>
 
 <div style = "background-color:blue; border:2px solid white; border-radius:25px; padding:1%; font-size:1.5em;" >
-<?php 
- $page = "deconnect.php";
-
-if($localhost == 0){
-
-$page = "/deconnect.php";
-
-}
-
-
-$link1 = $link.$page;
-
-?>
-
 
 <a  href = "./deconnect.php">deconnection</a>
 
