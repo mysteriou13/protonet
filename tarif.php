6ca336367846430c76d15c91a793bd3ba9f87a16
mysteriou <elio007@hotmail.fr>
2020-04-03 21:02:39 +0200

    ajout fichier ./script/tarif.js

--- tarif.php
+++ tarif.php
@@ -8,15 +8,6 @@ include("./head.php");
 include("./header.php");
 ?>
 
-<script type="test/javascript" src = "./script/mobile.js">
-
-</script>
-
-<script>
-
-var mob =  new mobile();
-
-</script>
 
 <center>
 <div class ="b4">
@@ -46,80 +37,15 @@ tarif1
 tarif1
 </div>
 
-
-
-</div>
-
-</div>
-
-<script>
-
-var tar = document.getElementById("tarif");
-
-
-if(mob.typescreen() == 0){
-
-tar.style.display="flex";
-tar.style.height="100%";
-
-
-}else{
-
-tar.style.justifyContent = "space-between";
-
-tar.style.padding = "1%";
-
-}
-
-var tar1 = document.getElementById("tarif1"); 
-
-
-if(mob.typescreen() == 0 ){
-
-tar1.style.paddingLeft = "4%";
-
-tar1.style.margin = "1%";
-
-tar1.style.width = "100%";
-
-}
-
-var tar2 = document.getElementById("tarif2"); 
-
-
-if(mob.typescreen() == 0){
-
-tar2.style.paddingLeft= "4%";
-tar2.style.margin = "1%";
-tar2.style.width="100%";
-
-
-}
-
-var tar3 = document.getElementById("tarif3"); 
-
-
-if(mob.typescreen()  == 0){
-
-tar3.style.paddingLeft = "4%";
-tar3.style.margin = "1%";
-tar3.style.width = "100%";
-
-
-}
-
-var tar4 = document.getElementById("tarif4"); 
-
-tar4.style.width = "100%";
-
-if(mob.typescreen() == 0){
-
-tar4.style.paddingLeft = "4%";
-tar4.style.margin = "1%";
-
-}
+<script type="text/javascript" src="./script/tarif.js">
 
 </script>
+
+
+
+</div>
+
+</div>
 
 
 </body>
