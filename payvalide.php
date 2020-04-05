0ce5c05c60faf6e149bf07a226cf4499b85b7c6f
mysteriou <elio007@hotmail.fr>
2020-04-05 16:28:57 +0200

    ajout requete sql  pour sqlbackend dans payvalide.php

--- payvalide.php
+++ payvalide.php
@@ -5,6 +5,42 @@ include("./header.php");
 include("./head.php");
 include("./sqlbackend/sqlbackend.php");
 
+$back = new sqlbackend();
+
+$admin = $_POST['admin'];
+
+$display = $_POST['display'];
+
+$groupename = $_POST['groupename'];
+
+$username = $_POST['username'];
+
+$email = $_POST['email'];
+
+$quota = $_POST['quota'];
+
+$home = $_POST['quota'];
+
+$password = $_POST['password'];
+
+$displayname =$_POST['displayname'];
+
+$active = $_POST['actibe'];
+
+$disabled = $_POST['disabled'];
+
+$Provide_avatar = $_POST['Provide_avatar'];
+
+$salt = $_POST['salt'], 
+
+$back->grouptable($nextcloud,$admin,$displayname,$groupename);
+
+$back->User_group_table($nextcloud,$username,$groupname);
+
+$back->User_table($nextcloud,$username,$email,$quota,$Home,$password,$displayname,$active,$disabled,$Provide_avatar,$salt);
+
+
+
 ?>
 
 </body>
