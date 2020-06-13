a8e7388d87be837010f2cadfa4a39496d8c826b6
mysteriou <elio007@hotmail.fr>
2020-06-13 14:32:40 +0200

    ajout newcompter.php

---
+++ newcompte.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<?php 
+
+include("./head.php");
+include("./header.php");
+
+if(empty($_SESSION['pseudo'])){
+
+header("Location:index.php");
+
+}
+
+?>
+
+<div id = "b4">
+
+<center>
+
+le paiment automatique &eacute;tais en cour de devellopement
+pour  toute nouvelle commande utliser le formualaire de contact 
+</br>
+merci de votre compr&eacute;hension
+
+</center>
+
+</div>
+
+</body>
+</html>
